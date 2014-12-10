20379385460dbceaf0c60b2a88b4c8615be8bd0a
nimisha04 <nimisha.mishra04@gmail.com>
2014-12-10 16:51:51 +0530

    title added on static pages:chapter-3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def sampleapp
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/sampleapp'
 
   # Example resource route with options:
   #   resources :products do
